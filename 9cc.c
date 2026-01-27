b213b94e481924424e87de0f48a29686051f7105
fuyu <54523771+mfmfuyu@users.noreply.github.com>
2026-01-27 21:50:06 +0900

    feat: improve error message

--- 9cc.c
+++ 9cc.c
@@ -20,12 +20,27 @@ struct Token {
 	char *str;
 };
 
+char *user_input;
+
 Token *token;
 
 void error(char *fmt, ...)
 {
 	va_list ap;
 	va_start(ap, fmt);
+	vfprintf(stderr, fmt, ap);
+	fprintf(stderr, "\n");
+	exit(1);
+}
+
+void error_at(char *loc, char *fmt, ...) {
+	va_list ap;
+	va_start(ap, fmt);
+
+	int pos = loc - user_input;
+	fprintf(stderr, "%s\n", user_input);
+	fprintf(stderr, "%*s", pos, " ");
+	fprintf(stderr, "^ ");
 	vfprintf(stderr, fmt, ap);
 	fprintf(stderr, "\n");
 	exit(1);
@@ -42,14 +57,14 @@ bool consume(char op)
 void expect(char op)
 {
 	if (token->kind != TK_RESERVED || token->str[0] != op)
-			error("expected '%c'", op);
+			error_at(token->str, "expected '%c'", op);
 	token = token->next;
 }
 
 int expect_number()
 {
 	if (token->kind != TK_NUM)
-		error("expected a number");
+		error_at(token->str, "expected a number");
 	int val = token->val;
 	token = token->next;
 	return val;
@@ -69,8 +84,9 @@ Token *new_token(TokenKind kind, Token *cur, char *str)
 	return tok;
 }
 
-Token *tokenize(char *p)
+Token *tokenize()
 {
+	char *p = user_input;
 	Token head;
 	head.next = NULL;
 	Token *cur = &head;
@@ -92,7 +108,7 @@ Token *tokenize(char *p)
 			continue;
 		}
 
-		error("invalid token");
+		error_at(p, "expected a number");
 	}
 
 	new_token(TK_EOF, cur, p);
@@ -106,17 +122,18 @@ int main(int argc, char **argv)
 		return 1;
 	}
 
-	token = tokenize(argv[1]);
+	user_input = argv[1];
+	token = tokenize();
 
 	printf(".intel_syntax noprefix\n");
 	printf(".globl main\n");
 	printf("main:\n");
 	
-	printf("	mov rax, %ld\n", expect_number());
+	printf("	mov rax, %d\n", expect_number());
 
 	while (!at_eof()) {
 		if (consume('+')) {
-			printf("	add rax, %ld\n", expect_number());
+			printf("	add rax, %d\n", expect_number());
 			continue;
 		}
 
